e4b8400ca5a0874bdf68f7599de79d0fd4c14bc9
yuyaebean <yuyaebean@gmail.com>
2019-12-08 00:48:46 +0900

    Add MySQL connection to port

--- models/management.js
+++ models/management.js
@@ -8,6 +8,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   timezone: 'Z',
   insecureAuth: true,
 })
